80da8d7fdad7ee35efba72d83527cf195b1fca11
CodingWithKarim <karimhassan92@gmail.com>
2025-01-11 23:44:21 -0500

    remove local .env

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 
 func init() {
 	// Load .env file in local environment
-	if err := godotenv.Load(); err != nil {
+	if err := godotenv.Load("/postoenv/.env"); err != nil {
 		log.Fatal(".env file not found")
 	}
 }
